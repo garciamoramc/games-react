e52109258672f0d069cb243d666a6fc043d808fe
Mari Carmen <160607449+garciamoramc@users.noreply.github.com>
2024-02-21 17:03:50 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ function AppCover() {
       </nav>
 
       <div className="cover-content">
-        <h1>Welcome!b</h1>
-        <p>Discover a variety of mini-games created with React and aimed at practising <a href='https://reactrouter.com/en/main' target="_blank">React Router</a>.</p>
+        <h1>Welcome!</h1>
+        <p>Discover a variety of mini-games created with React and aimed at practising <a href='https://reactrouter.com/en/main' target="_blank">React Router</a>.<br></p>
         <p>Explore the different games and have fun!</p>
       </div>
     </div>
